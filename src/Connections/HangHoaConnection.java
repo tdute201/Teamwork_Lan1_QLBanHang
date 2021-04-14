daf93cff30cc852cc04025b5515741c7b837a749
NguyenThiNhu-Y <nhuyhe62001@gmail.com>
2021-04-14 22:19:13 +0700

    delete

--- src/Connections/HangHoaConnection.java
+++ src/Connections/HangHoaConnection.java
@@ -5,10 +5,29 @@
  */
 package Connections;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author thien
  */
 public class HangHoaConnection {
-    
+        static JDBCConnection con= new JDBCConnection();
+
+    public static void delete(String idSP){
+        Connection cn = con.getJDBCConnection();
+        String sql = "delete from tblSanPham where idSP = ?";
+        try {
+            PreparedStatement pstm = cn.prepareStatement(sql);
+            pstm.setString(1, idSP);
+            int rs = pstm.executeUpdate();
+            System.out.println(rs);
+        } catch (SQLException ex) {
+            Logger.getLogger(HangHoaConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
