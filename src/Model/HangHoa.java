27b52767df0e8c665eaed81200a5a4a59235e0da
NguyenThiNhu-Y <nhuyhe62001@gmail.com>
2021-04-14 22:08:14 +0700

    Tao model

---
+++ src/Model/HangHoa.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author 84327
+ */
+public class HangHoa {
+    private String maHang;
+    private String tenMatHang;
+    private String namSanXuat;
+    private String donGian;
+    private String soLuong;
+
+    public String getMaHang() {
+        return maHang;
+    }
+
+    public void setMaHang(String maHang) {
+        this.maHang = maHang;
+    }
+
+    public String getTenMatHang() {
+        return tenMatHang;
+    }
+
+    public void setTenMatHang(String tenMatHang) {
+        this.tenMatHang = tenMatHang;
+    }
+
+    public String getNamSanXuat() {
+        return namSanXuat;
+    }
+
+    public void setNamSanXuat(String namSanXuat) {
+        this.namSanXuat = namSanXuat;
+    }
+
+    public String getDonGian() {
+        return donGian;
+    }
+
+    public void setDonGian(String donGian) {
+        this.donGian = donGian;
+    }
+
+    public String getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(String soLuong) {
+        this.soLuong = soLuong;
+    }
+    
+}
